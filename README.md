4e25423f0ff10357bd6fa23bb47826cb22925092
TerraAr <carames@ufpr.br>
2020-06-18 15:16:51 -0300

    Melhorias gerais
     * Mudado nome do arquivo para lista.cpp
     * Melhorada indentação geral da biblioteca
     * Atualização do README
     * Adição de um comando para pré-compilador para garantir que o marcador
       inline seja respeitado

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # lista
 Protópipo de classe de lista lincada.
+
+## Operadores
+ - `[unsigned long long pos]`
+   Retorna a posição da lista linkada como variável endereço.
+
+## Funções
+ - construtor `lista(Type a=0)`
+   Cria a lista e inicializa a primeira posição com o valor recebido
+ - destruidor `~lista()`
+   Desaloca as posições alocadas dinamicamente e destrói as variáveis estáticas.
+ - `gotopos(unsigned long long pos)`
+   Manda o cursor da lista para a posição especificada.
+ - `escreve(Type a)`
+   Escreve o valor mandado na posição atual da lista.
+ - `Type le()`
+   Retorna a posição atual da lista.
---
+++ lista.cpp
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+static const char* fora_da_lista = "ERRO: Posição inexistente.\n";
+
+const size_t TAMVOID=sizeof(void*);
+
+template <class Type> class lista{
+	private:
+		void *inicio, *PosicaoAtual;
+		unsigned long long posicao, fim;
+		const size_t TamanhoTipo;
+
+		inline void gotox(unsigned long long x){
+			if(x < posicao){
+				posicao = 0ULL;
+				PosicaoAtual = inicio;
+			}
+			for(; x > posicao; posicao++)
+				PosicaoAtual = *(void**)PosicaoAtual;
+		}
+
+	public:
+		lista(Type a = 0ULL) : TamanhoTipo(sizeof(Type) + TAMVOID){
+			inicio = malloc(TamanhoTipo);
+			PosicaoAtual = inicio;
+			posicao = fim = 0ULL;
+			* (Type*) (PosicaoAtual+TAMVOID) = a;
+		}
+
+		~lista(){
+			while(fim > 0ULL){
+				gotox(fim--);
+				free(PosicaoAtual);
+			}
+			free((void*)inicio);
+		}
+
+		void gotopos(unsigned long long x){
+			if(x > fim){
+				fputs(fora_da_lista, stderr);
+				return (void)1;
+			}
+			if(x < posicao){
+				posicao = 0ULL;
+				PosicaoAtual = (void*)inicio;
+			}
+			for(; x>posicao; posicao++){
+				PosicaoAtual = *(void**)PosicaoAtual;
+			}
+			return (void)0;
+			}
+
+			void addpos(unsigned long long a){
+				for(; a>0ULL; fim++, a--){
+					gotox(fim);
+					*(void**)PosicaoAtual =
+						malloc(TamanhoTipo);
+				}
+			}
+
+			void escreve(Type a){
+				*((Type*)(PosicaoAtual+TAMVOID)) = a;
+			}
+			Type le() {return * (Type*) (PosicaoAtual + TAMVOID);}
+
+			inline Type& operator[](const unsigned long long)
+						__attribute__((always_inline));
+};
+
+template <class Type> inline Type& lista<Type>::operator[]
+					(const unsigned long long pos){
+	gotopos(pos);
+	return * (Type*) (PosicaoAtual + TAMVOID);
+}
